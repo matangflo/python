5f5dd4f7464402621d52663869b362eafa46a264
USUARIO <USUARIO@10.100.219.73>
2021-11-08 14:08:22 +0100

    1º entrega corregida

--- src/Jugadores_NBA_test.py
+++ src/Jugadores_NBA_test.py
@@ -7,9 +7,9 @@ from Jugadores_NBA import *
 ################################################################
 def main():
     lista_jugadores=lee_jugadores("C:/Users/USUARIO/git/proyecto-entregable-python-matangflo/data/Dataset_NBA.csv")
-    print(len (lista_jugadores))
-    print(lista_jugadores[:3])
-    print(lista_jugadores[-3:])
+    print("Hay",len (lista_jugadores), "jugadores")
+    print("3 primeros jugadores",lista_jugadores[:3])
+    print("3 ultimos jugadores",lista_jugadores[-3:])
 
 if __name__=="__main__":
     main()
